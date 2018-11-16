03da0f70cd12d06712052c86e149d8bbef451364
mohamedaslam28 <mohamedaslam28@gmail.com>
2018-11-16 11:20:44 +0800

    New changes

--- IntretechGithub/IntretechGithub/ViewController.m
+++ IntretechGithub/IntretechGithub/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     NSLog(@"Print First log");
+    //New Changes
     // Do any additional setup after loading the view, typically from a nib.
 }
 
